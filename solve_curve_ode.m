1bfbb655ab8b2d4f15029545dcdd1d3ed43f351a
Ionut Pop <ionut.pop@mindmaze.ch>
2018-07-24 17:31:26 +0300

    draw it, probably ode is incorrect

--- solve_curve_ode.m
+++ solve_curve_ode.m
@@ -3,10 +3,18 @@ xdot_0 = @(r, o) [o, (alpha*(r^4 + 2*r^3*o) - o*r^3 - 2*r*o^3)/(alpha*r^3 - o*r^
 
 xdot = @(x, t) xdot_0(x(1), x(2))
 
-x0 = [1, 0];
+x0 = [1, -1];
 
-xdot(x0, 1)
+disp('xdot(x0)='), disp(xdot(x0, 1))
 
-t = linspace(0, 2*pi, 1000);
-[x, state, msg] = lsode(xdot, x0, t)
+t = linspace(0, 1.6, 100);
+[x, state, msg] = lsode(xdot, x0, t);
+
+disp(msg);
+
+points = [x(:, 1), x(:, 1)] .* [cos(t'), sin(t')]
+
+plot(points(:, 1), points(:, 2))
+
+pause()
 
